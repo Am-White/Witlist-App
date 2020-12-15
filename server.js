b4156371e4d7130f938495ed058c2b1de282eea6
littlefire <aybwhite@gmail.com>
2020-12-14 20:30:06 -0800

    port fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 //Setting up Express
 //=================
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000 ;
 
 //Express and data parsing??
 //=================
