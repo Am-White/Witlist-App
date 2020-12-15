b9d68557b0e19d45d542768657274c8d4431391f
littlefire <aybwhite@gmail.com>
2020-12-14 19:37:05 -0800

    server.js up and working// linked to html files // routes connected

---
+++ server.js
@@ -0,0 +1,35 @@
+//Dependencies
+//=================
+const express = require("express");
+const path = require("path");
+
+//Setting up Express
+//=================
+const app = express();
+const PORT = 3000;
+
+//Express and data parsing??
+//=================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//Routes???
+//=================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+//Listener
+//=================
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
