78c8fc98eb97dc24aa0fa2830bde456179819f16
littlefire <aybwhite@gmail.com>
2020-12-14 20:05:48 -0800

    finished html files

---
+++ waitlist.js
@@ -0,0 +1,14 @@
+//DATA TO HOLD PLACEMENT
+
+const tableArray = [
+    {
+        customerName: "John Doe",
+        customerEmail: "john.doe@gmail.com",
+        customerID: "J.D.",
+        phoneNumber: "123-456-7890"
+    }
+];
+
+
+
+module.exports = tableArray;
